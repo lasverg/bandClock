a819aa5a25d1b2d11a5a9a9c2fa75334f3900dfb
Nitin Jha <zaniitiin@gmail.com>
2015-10-08 17:51:56 +0530

    update

---
+++ script/script.js
@@ -0,0 +1,14 @@
+$(function(){
+    $(document).ready(function(){
+        $('.bclock').bandClock({
+            // Custom Properties Here
+            size: 280,
+        });
+        $('.wclock').bandClock({
+            // Custom Properties Here
+            bgColor: '#E0E0E0',
+            size: 280,
+            color: '#D81B60',
+        });
+    });
+})();
